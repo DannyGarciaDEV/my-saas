9a0661cc3f2f02b64d548bdf1788fcd673efcb47
Danny Garcia Cortes <dannygarciadev@gmail.com>
2024-03-07 13:07:10 -0500

    adding wasp, migration db

---
+++ app/.wasp/out/sdk/wasp/dist/ext-src/client/components/Testing.d.ts
@@ -0,0 +1,3 @@
+import React from 'react';
+declare const Testing: React.FC;
+export default Testing;
---
+++ app/.wasp/out/sdk/wasp/dist/ext-src/client/components/Testing.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+const Testing = () => {
+    return (<div>
+      <h1>Hello, World!</h1>
+    </div>);
+};
+export default Testing;
+//# sourceMappingURL=Testing.jsx.map
